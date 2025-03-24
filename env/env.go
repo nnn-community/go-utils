259097000e0211df1ef055da0b80232047b5e9bb
Patrik Krehák <patrik@krehak.com>
2025-03-24 17:14:01 +0100

    load env from WD

--- env/env.go
+++ env/env.go
@@ -4,39 +4,52 @@ import (
     "fmt"
     "log"
     "os"
+    "path/filepath"
 
     "github.com/joho/godotenv"
 )
 
 func Load() {
+    wd, err := os.Getwd()
+
+    if err != nil {
+        log.Fatal("Error getting working directory")
+    }
+
     env := os.Getenv("APP_ENV")
 
     if env == "" {
         env = "development"
     }
 
-    if err := godotenv.Load(); err != nil {
+    if err := godotenv.Load(filepath.Join(wd, ".env")); err != nil {
         log.Fatal("Error loading .env file")
     }
 
-    if err := godotenv.Load(fmt.Sprintf(".env.%s", env)); err != nil {
+    if err := godotenv.Load(filepath.Join(wd, fmt.Sprintf(".env.%s", env))); err != nil {
         log.Fatal(fmt.Sprintf("Error loading .env.%s file", env))
     }
 
-    if _, err := os.Stat(".env.local"); err == nil {
-        if err := godotenv.Load(".env.local"); err != nil {
+    if _, err := os.Stat(filepath.Join(wd, ".env.local")); err == nil {
+        if err := godotenv.Load(filepath.Join(wd, ".env.local")); err != nil {
             log.Fatal("Error loading .env.local file")
         }
     }
 }
 
 func LoadSingle() {
-    if err := godotenv.Load(); err != nil {
+    wd, err := os.Getwd()
+
+    if err != nil {
+        log.Fatal("Error getting working directory")
+    }
+
+    if err := godotenv.Load(filepath.Join(wd, ".env")); err != nil {
         log.Fatal("Error loading .env file")
     }
 
-    if _, err := os.Stat(".env.local"); err == nil {
-        if err := godotenv.Load(".env.local"); err != nil {
+    if _, err := os.Stat(filepath.Join(wd, ".env.local")); err == nil {
+        if err := godotenv.Load(filepath.Join(wd, ".env.local")); err != nil {
             log.Fatal("Error loading .env.local file")
         }
     }
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/nnn-community/go-utils
 go 1.21.5
 
 require (
-	github.com/gorilla/websocket v1.5.3 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/gorilla/websocket v1.5.3
+	github.com/joho/godotenv v1.5.1
 )
